2c2ec3cea0803a69d7056f7389ea10a9bb2a4af1
lior.kashi <liorka2@mta.ac.il>
2026-03-20 18:48:58 +0200

    bug in loging

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,23 +4,15 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in search params, use it as the redirection URL
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
+    
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // confirmation_url is on separate domain
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that origin is localhost
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      // If we are on Vercel, use the request origin instead of hardcoded localhost
+      return NextResponse.redirect(`${origin}${next}`)
     }
   }
 
